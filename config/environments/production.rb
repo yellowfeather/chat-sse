2a81b579fc8f7b78fec33546fc1d5b46415e79b8
Chris Richards <chris.richards@yellowfeather.co.uk>
2014-06-16 22:38:23 +0100

    Initialises config.logger to STDOUT

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -44,6 +44,7 @@ ChatSse::Application.configure do
   # config.force_ssl = true
 
   # Set to :debug to see everything in the log.
+  config.logger = Logger.new(STDOUT)
   config.log_level = :info
 
   # Prepend all log lines with the following tags.
